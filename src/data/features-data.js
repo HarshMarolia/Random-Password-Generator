1f2b4a9e4323dd2af4509331fb5a56b81fb0c1ee
HarshMarolia <maroliaharsh@gmail.com>
2023-09-25 14:17:51 +0530

    Bhoomi AI

---
+++ src/data/features-data.js
@@ -0,0 +1,37 @@
+import image1 from '../img/image1.jpg';
+import image2 from '../img/image2.jpg';
+import image3 from '../img/image3.jpg';
+import image4 from '../img/image4.jpg';
+
+export const featuresData = [
+  {
+    color: "blue",
+    title: "Pesticide Check",
+    icon: image1,
+    description:
+      "Our Machine Learning model detects the harmful insect and suggest a solution to treat them.",
+  },
+  {
+    color: "red",
+    title: "Talk to Expert",
+    icon: image2,
+    description:
+      "Talk to our expert agricultural scientists and researcher to solve your specific requirements..",
+  },
+  {
+    color: "teal",
+    title: "Crop Health",
+    icon: image3,
+    description:
+      "Estimation of the crop health using the few images of the crop.",
+  },
+  {
+    color: "teal",
+    title: "Soil Health",
+    icon: image4,
+    description:
+      "Soil nutrient, its features and suitable crop suggestion based on few images of your Crop.",
+  },
+];
+
+export default featuresData;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export * from "@/pages/home";
+export * from "@/pages/profile";
+export * from "@/pages/services";
+export * from "@/pages/sign-in";
+export * from "@/pages/sign-up";
